bc2da4d22eb0552b535204aea83ba392f4f9913d
Himani Mishra <himanimishra07@gmail.com>
2019-10-01 23:03:27 -0700

    adding algorithms on graphs

---
+++ algorithms_on_graphs/Reachability.cs
@@ -0,0 +1,37 @@
+﻿// C# program to check if a destination is  
+// reachable from source with two movements  
+// allowed 
+using System;
+
+class Reachability
+{
+
+    static bool isReachable(int sx, int sy,
+                             int dx, int dy)
+    {
+
+        // base case 
+        if (sx > dx || sy > dy)
+            return false;
+
+        // current point is equal to destination 
+        if (sx == dx && sy == dy)
+            return true;
+
+        // check for other 2 possibilities 
+        return (isReachable(sx + sy, sy, dx, dy) ||
+                isReachable(sx, sy + sx, dx, dy));
+    }
+
+    //driver code 
+    public static void Main()
+    {
+        int source_x = 2, source_y = 10;
+        int dest_x = 26, dest_y = 12;
+        if (isReachable(source_x, source_y, dest_x,
+                                           dest_y))
+            Console.Write("True\n");
+        else
+            Console.Write("False\n");
+    }
+}
---
+++ algorithms_on_graphs/finding_two_points_in_a_straight_line.cs
@@ -0,0 +1,41 @@
+﻿// C# Program to find m and c for a 
+// straight line given, x and y 
+using System;
+
+class finding_straight_line
+{
+
+    // function to calculate m and c that 
+    // best fit points represented by x[] and y[] 
+    static void bestApproximate(int[] x, int[] y)
+    {
+        int n = x.Length;
+        double m, c, sum_x = 0, sum_y = 0,
+                     sum_xy = 0, sum_x2 = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            sum_x += x[i];
+            sum_y += y[i];
+            sum_xy += x[i] * y[i];
+            sum_x2 += Math.Pow(x[i], 2);
+        }
+
+        m = (n * sum_xy - sum_x * sum_y) / (n * sum_x2 - Math.Pow(sum_x, 2));
+
+        c = (sum_y - m * sum_x) / n;
+
+        Console.WriteLine("m = " + m);
+        Console.WriteLine("c = " + c);
+    }
+
+    // Driver main function 
+    public static void Main()
+    {
+        int[] x = { 1, 2, 3, 4, 5 };
+        int[] y = { 14, 27, 40, 55, 68 };
+
+        // Function calling 
+        bestApproximate(x, y);
+    }
+}
