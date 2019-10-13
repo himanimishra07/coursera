fcf0fb0de7fbc3586f570ab0964c82976c3ccc8f
Himani Mishra <himanimishra07@gmail.com>
2019-10-12 23:50:56 -0700

    assignment_on_strings

---
+++ algorithms_on_strings/KMP_pattern_searching/ConsoleApp1/Program.cs
@@ -0,0 +1,92 @@
+﻿// C# program for implementation of KMP pattern 
+// searching algorithm 
+using System;
+
+class KMP_pattern_searching
+{
+    public static void Main()
+    {
+        string txt = "ABABDABACDABABCABAB";
+        string pat = "ABABCABAB";
+        new KMP_pattern_searching().KMPSearch(pat, txt);
+    }
+    void KMPSearch(string pat, string txt)
+    {
+        int M = pat.Length;
+        int N = txt.Length;
+
+        // create lps[] that will hold the longest 
+        // prefix suffix values for pattern 
+        int[] lps = new int[M];
+        int j = 0; // index for pat[] 
+
+        // Preprocess the pattern (calculate lps[] 
+        // array) 
+        computeLPSArray(pat, M, lps);
+
+        int i = 0; // index for txt[] 
+        while (i < N)
+        {
+            if (pat[j] == txt[i])
+            {
+                j++;
+                i++;
+            }
+            if (j == M)
+            {
+                Console.Write("Found pattern "
+                              + "at index " + (i - j));
+                j = lps[j - 1];
+            }
+
+            // mismatch after j matches 
+            else if (i < N && pat[j] != txt[i])
+            {
+                // Do not match lps[0..lps[j-1]] characters, 
+                // they will match anyway 
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i = i + 1;
+            }
+        }
+    }
+
+    void computeLPSArray(string pat, int M, int[] lps)
+    {
+        // length of the previous longest prefix suffix 
+        int len = 0;
+        int i = 1;
+        lps[0] = 0; // lps[0] is always 0 
+
+        // the loop calculates lps[i] for i = 1 to M-1 
+        while (i < M)
+        {
+            if (pat[i] == pat[len])
+            {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else // (pat[i] != pat[len]) 
+            {
+               
+                if (len != 0)
+                {
+                    len = lps[len - 1];
+
+                    // Also, note that we do not increment 
+                    // i here 
+                }
+                else // if (len == 0) 
+                {
+                    lps[i] = len;
+                    i++;
+                }
+            }
+        }
+    }
+
+   
+   
+}
